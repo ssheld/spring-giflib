b2d726462f7b6d2a9c2cc01d00bc8eeb11cb70e3
Stephen <sheldon2521@gmail.com>
2019-03-19 18:10:59 -0400

    Added links on homepage

--- src/main/java/com/ssheld/giflib/controller/GifController.java
+++ src/main/java/com/ssheld/giflib/controller/GifController.java
@@ -4,9 +4,10 @@ import com.ssheld.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Author: Stephen Sheldon 3/18/2019
@@ -21,14 +22,16 @@ public class GifController {
     private GifRepository gifRepository = new GifRepository();
 
     @RequestMapping("/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
-    @RequestMapping("/gif")
+    @RequestMapping("/gif/{name}")
     // Add it to a ModelMap so that we can add it to the gif-details view.
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = gifRepository.findByName("android-explosion");
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         // Add our gif to the ModelMap object that was passed in.
         modelMap.put("gif", gif);
         // There is no need to return the ModelMap as the Spring Framework will
